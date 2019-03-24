43f295f8445c2fb2d8622a5b2dd016b6c276ddc7
Jamorx <1025454885@qq.com>
2019-03-24 14:48:41 +0800

    yl

--- src/Equipment/Equipment.java
+++ src/Equipment/Equipment.java
@@ -5,4 +5,20 @@ abstract public class Equipment {
     int Atk;
 
     int Def;
+
+    public int getAtk() {
+        return Atk;
+    }
+
+    public void setAtk(int atk) {
+        Atk = atk;
+    }
+
+    public int getDef() {
+        return Def;
+    }
+
+    public void setDef(int def) {
+        Def = def;
+    }
 }
